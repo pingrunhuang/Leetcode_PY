11f74d909b3c304f1ce4960b2199c91df55e1254
FrankHuang <pingrunhuang@gmail.com>
2024-04-26 17:28:16 +0800

    initial commit

---
+++ src/leetcode_py/198.py
@@ -0,0 +1,61 @@
+"""
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+DP FIBONACCI
+"""
+class Solution(object):
+    def rob(self, nums):
+        """
+        time limit exceed
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        return max(nums[0]+self.rob(nums[2:]), nums[1]+self.rob(nums[3:]))
+
+
+class Solution2(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        self.memo = {0: nums[0], -1:0}
+        for i in range(1, n):
+            self.memo[i] = max(nums[i] + self.memo[i-2], self.memo[i-1])
+        return self.memo[n-1]
+
+
+if __name__ == "__main__":
+    solution = Solution2()
+    t = [2,7,9,3,1]
+    print(solution.rob(t))
+    t = [1,2]
+    print(solution.rob(t))
+    t = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    print(solution.rob(t))
